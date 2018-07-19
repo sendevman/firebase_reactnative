b37696748a7e1ddaa369822e4e611fc59b8f5b76
nativelocation <bluesky6460@mail.com>
2018-07-19 12:50:44 +1000

    Device id added

--- android/app/src/main/java/com/retailcompanion/MainApplication.java
+++ android/app/src/main/java/com/retailcompanion/MainApplication.java
@@ -3,6 +3,7 @@ package com.retailcompanion;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.ninty.system.setting.SystemSettingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new SystemSettingPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
