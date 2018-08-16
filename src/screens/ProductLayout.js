13ac6d0f1f55f33735d849217ede20bea01d4f9a
sun <vbaut0730@yahoo.com>
2018-08-17 03:53:40 +1000

    Fix [On Boarding] Only show 1st time

--- src/screens/ProductLayout.js
+++ src/screens/ProductLayout.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from 'react';
-import { Animated, Dimensions, TouchableHighlight, View } from 'react-native';
+import { Animated, Dimensions, TouchableHighlight, AsyncStorage } from 'react-native';
 import { createStackNavigator, SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 import firebase from 'react-native-firebase';
@@ -33,11 +33,16 @@ class ProductLayoutScreen extends Component {
     this.state = {
       animatedValue: new Animated.Value(0)
     }
-  }
+    try {
+      AsyncStorage.setItem('passOnboarding', 'passed');
+    } catch (error) {
+      // Error saving data
+    }
+}
 
-  getProductID(zone_id) {
-    const areasRef = firebase.firestore().collection('areas');
-    areasRef.where('id', '==', zone_id).get()
+getProductID(zone_id) {
+  const areasRef = firebase.firestore().collection('areas');
+  areasRef.where('id', '==', zone_id).get()
     .then(snapshot => {
       const arrAreas = snapshot.docs.map(doc => doc.data());
       this.props.dispatch(setAreaInfo(arrAreas));
@@ -45,16 +50,16 @@ class ProductLayoutScreen extends Component {
         this.getFirstProductDetail(arrAreas);
       }
     });
-  }
+}
 
-  getFirstProductDetail(arrAreas) {
-    if (arrAreas[0] != undefined) {
-      const arrproducts = arrAreas[0].products;
-      const productRef = firebase.firestore().collection('products');
+getFirstProductDetail(arrAreas) {
+  if (arrAreas[0] != undefined) {
+    const arrproducts = arrAreas[0].products;
+    const productRef = firebase.firestore().collection('products');
 
-      Promise.all(
-        arrproducts.map(productID => new Promise((resolve, reject) => {
-          productRef.doc(productID).get()
+    Promise.all(
+      arrproducts.map(productID => new Promise((resolve, reject) => {
+        productRef.doc(productID).get()
           .then(product => {
             if (!product.exists) resolve({});
 
@@ -62,105 +67,116 @@ class ProductLayoutScreen extends Component {
             productData.id = productID;
 
             productRef.doc(productID).collection('web-reviews').get()
-            .then(reviews => {
-              var webReviews = [];
+              .then(reviews => {
+                var webReviews = [];
 
-              reviews.forEach(review => {
-                const reviewData = review.data();
-                webReviews.push(reviewData);
-              });
+                reviews.forEach(review => {
+                  const reviewData = review.data();
+                  webReviews.push(reviewData);
+                });
 
-              productData.webReviews = webReviews.sort((a, b) => {
-                if (a.position < b.position) return -1;
-                if (a.position > b.position) return 1;
-                return 0;
-              });
+                productData.webReviews = webReviews.sort((a, b) => {
+                  if (a.position < b.position) return -1;
+                  if (a.position > b.position) return 1;
+                  return 0;
+                });
 
-              resolve(productData);
-            })
-            .catch(err => { console.log('Error getting documents', err); });
+                resolve(productData);
+              })
+              .catch(err => { console.log('Error getting documents', err); });
           })
           .catch(error => reject(error));
-        }))
-      )
+      }))
+    )
       .then(results => {
         this.props.dispatch(setProductsNearInfo(results));
         this.props.dispatch(setProductInfo(results[0]));
         // console.log("log event ======= : ", {"pFirebaseId":this.props.firebaseid, "pDeviceModel":results[0].model, "pDeviceManufacture":results[0].manufacture, "pResearchTab":"info"});
         // firebase.analytics().logEvent("deviceViewed", {"pFirebaseId":this.props.firebaseid, "pDeviceModel":results[0].model, "pDeviceManufacture":results[0].manufacture, "pResearchTab":"info"});
-      
+
         setTimeout(() => this.forceUpdate(), 300);
       })
-      .catch(error => {})
-    } else {
-      // Set Null for Non-Zone
-    }
+      .catch(error => { })
+  } else {
+    // Set Null for Non-Zone
   }
+}
 
-  setCurrentProduct(productId) {
-    const { productsNear } = this.props;
-    if (!productsNear || productsNear.length === 0) return;
-    const match = productsNear.filter(product => product.id === productId);
-    this.props.dispatch(setProductInfo(match.length > 0 ? match[0] : {}));
-    // console.log("log event ======= : ", {"pFirebaseId":this.props.firebaseid, "pDeviceModel":match[0].model, "pDeviceManufacture":match[0].manufacture, "pResearchTab":"info"});
-    // firebase.analytics().logEvent("deviceViewed", {"pFirebaseId":this.props.firebaseid, "pDeviceModel":match[0].model, "pDeviceManufacture":match[0].manufacture, "pResearchTab":"info"});
-  }
+setCurrentProduct(productId) {
+  const { productsNear } = this.props;
+  if (!productsNear || productsNear.length === 0) return;
+  const match = productsNear.filter(product => product.id === productId);
+  this.props.dispatch(setProductInfo(match.length > 0 ? match[0] : {}));
+  // console.log("log event ======= : ", {"pFirebaseId":this.props.firebaseid, "pDeviceModel":match[0].model, "pDeviceManufacture":match[0].manufacture, "pResearchTab":"info"});
+  // firebase.analytics().logEvent("deviceViewed", {"pFirebaseId":this.props.firebaseid, "pDeviceModel":match[0].model, "pDeviceManufacture":match[0].manufacture, "pResearchTab":"info"});
+}
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.locationData.zone_id !== nextProps.locationData.zone_id) {
-      let zone_id = nextProps.locationData.zone_id;
-      this.props.dispatch(setProductInfo({}));
-      this.props.dispatch(setProductsNearInfo([]));
-      setTimeout(() => this.forceUpdate(), 100);
-      this.getProductID(zone_id);
-    }
+componentWillReceiveProps(nextProps) {
+  if (this.props.locationData.zone_id !== nextProps.locationData.zone_id) {
+    let zone_id = nextProps.locationData.zone_id;
+    this.props.dispatch(setProductInfo({}));
+    this.props.dispatch(setProductsNearInfo([]));
+    setTimeout(() => this.forceUpdate(), 100);
+    this.getProductID(zone_id);
   }
+}
 
   static navigationOptions = ({ navigation }) => {
-    let heightPiv = navigation.getParam('heightHeader');
-    let heightHeader = (typeof heightPiv === "undefined") ? 56 : heightPiv;
-    return {
-      headerTitle: <LogoTitle />,
-      header: props => <GradientHeader {...props} />,
-      headerStyle: { backgroundColor: 'transparent', overflow: 'hidden', height: heightHeader },
-      headerLeft: (
-        <TouchableHighlight style={{ marginLeft: 16 }} onPress={() => navigation.openDrawer()}>
-          <Icon name="Menu" width="24" height="24" fill="#FFF" viewBox="0 0 24 24" />
-        </TouchableHighlight>
-      ),
-      headerRight: (
-        <TouchableHighlight style={{ marginRight: 16 }} >
-          <Icon name="Menu" width="24" height="24" fill="transparent" viewBox="0 0 24 24" />
-        </TouchableHighlight>
-      )
-    };
+  let heightPiv = navigation.getParam('heightHeader');
+  let heightHeader = (typeof heightPiv === "undefined") ? 56 : heightPiv;
+  return {
+    headerTitle: <LogoTitle />,
+    header: props => <GradientHeader {...props} />,
+    headerStyle: { backgroundColor: 'transparent', overflow: 'hidden', height: heightHeader },
+    headerLeft: (
+      <TouchableHighlight style={{ marginLeft: 16 }} onPress={() => navigation.openDrawer()}>
+        <Icon name="Menu" width="24" height="24" fill="#FFF" viewBox="0 0 24 24" />
+      </TouchableHighlight>
+    ),
+    headerRight: (
+      <TouchableHighlight style={{ marginRight: 16 }} >
+        <Icon name="Menu" width="24" height="24" fill="transparent" viewBox="0 0 24 24" />
+      </TouchableHighlight>
+    )
   };
+};
 
-  zone() {
-    let data = {
-      lat:"35.000",
-      lng:"-80.000",
-      height:"1",
-      ts:"2018-07-09",
-      floor_id:"1348",
-      zone_id: 3902
-    }
-    let data1 = {
-      lat:"-35.000",
-      lng:"-80.000",
-      height:"-1",
-      ts:"2018-07-09",
-      floor_id:"-1348",
-      zone_id:3903
-    }
-    count++;
-    this.props.dispatch(setLocationData(count%2 === 0 ? data : data1));
+zone() {
+  let data = {
+    lat: "35.000",
+    lng: "-80.000",
+    height: "1",
+    ts: "2018-07-09",
+    floor_id: "1348",
+    zone_id: 3902
   }
+  let data1 = {
+    lat: "-35.000",
+    lng: "-80.000",
+    height: "-1",
+    ts: "2018-07-09",
+    floor_id: "-1348",
+    zone_id: 3903
+  }
+  count++;
+  this.props.dispatch(setLocationData(count % 2 === 0 ? data : data1));
+}
 
-  render() {
-    const { productsNear } = this.props;
+render() {
+  const { productsNear } = this.props;
 
-    const transform = [
+  const transform = [
+    {
+      translateY: this.state.animatedValue.interpolate({
+        inputRange: [0, 166],
+        outputRange: [0, -68],
+        extrapolate: 'clamp'
+      })
+    }
+  ];
+
+  let productsBox = {
+    transform: [
       {
         translateY: this.state.animatedValue.interpolate({
           inputRange: [0, 166],
@@ -168,45 +184,34 @@ class ProductLayoutScreen extends Component {
           extrapolate: 'clamp'
         })
       }
-    ];
-
-    let productsBox = {
-      transform: [
-        {
-          translateY: this.state.animatedValue.interpolate({
-            inputRange: [0, 166],
-            outputRange: [0, -68],
-            extrapolate: 'clamp'
-          })
-        }
-      ]/*,
+    ]/*,
       height: this.state.animatedValue.interpolate({
         inputRange: [0, 166],
         outputRange: [height - 244, height - 176],
         extrapolate: 'clamp'
       })*/
-    };
+  };
 
-    const headerHeight = this.state.animatedValue.interpolate({
-      inputRange: [0, 166],
-      outputRange: [120, 52],
-      extrapolate: 'clamp'
-    });
+  const headerHeight = this.state.animatedValue.interpolate({
+    inputRange: [0, 166],
+    outputRange: [120, 52],
+    extrapolate: 'clamp'
+  });
 
-    return (
-      <SafeAreaView forceInset={{ top: 'always' }} style={{ backgroundColor: '#FFF' }}>
-        <ProductsNearSlide
-          animatedValue={this.state.animatedValue}
-          onProductIdChange={productId => this.setCurrentProduct(productId)}
-          currentProducts={productsNear}
-          zone={this.zone.bind(this)} />
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }} style={{ backgroundColor: '#FFF' }}>
+      <ProductsNearSlide
+        animatedValue={this.state.animatedValue}
+        onProductIdChange={productId => this.setCurrentProduct(productId)}
+        currentProducts={productsNear}
+        zone={this.zone.bind(this)} />
 
-        <Animated.View style={[{ width: '100%', height: height - 176 }, productsBox]}>
-          <RoutesProducts onScrollLayout={this.state.animatedValue} />
-        </Animated.View>
-      </SafeAreaView>
-    );
-  }
+      <Animated.View style={[{ width: '100%', height: height - 176 }, productsBox]}>
+        <RoutesProducts onScrollLayout={this.state.animatedValue} />
+      </Animated.View>
+    </SafeAreaView>
+  );
+}
 }
 
 const mapStateToProps = state => {
