c091b8b0a18e2fe80d73d7ecf8a636fed99c3a25
nativelocation <bluesky6460@mail.com>
2018-10-19 06:07:27 +1000

    get all location data

---
+++ src/actions/Locations.js
@@ -0,0 +1,11 @@
+/**
+ * Conexus-Tech - Retail Companion AT&T
+ * https://conexustech.com/
+ * @flow
+ */
+
+  export const setLocationAllInfo = (params) => ({
+    type: 'STORE_LOCATION_ALL_INFO',
+    payload: params
+  })
+  
