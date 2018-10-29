524382716a5fb67a4c0f3bf127af3e9fd697757c
mdev0730 <hashming0730@yahoo.com>
2018-10-30 00:20:38 +1000

    Fix navigation error

--- src/screens/CompareLayout.js
+++ src/screens/CompareLayout.js
@@ -5,11 +5,12 @@
  */
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { createStackNavigator, SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 
 // My Screens
+import Icon from '../assets/images/Icon';
 import CompareScreen from '../screens/CompareScreen';
 
 const RoutesCompare = createStackNavigator(
@@ -31,6 +32,11 @@ class CompareLayout extends Component {
       <SafeAreaView forceInset={{ top: 'always' }} style={{ backgroundColor: '#FFF' }}>
         <View style={{ width: '100%', height: '100%' }}>
           <RoutesCompare />
+          <View style={{ position: 'absolute', top: 5, left: 10 }}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('TabNav')}>
+              <Icon name="ArrowLeft" width="10" height="16" viewBox="0 0 10 16" fill="#333" />
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     );
