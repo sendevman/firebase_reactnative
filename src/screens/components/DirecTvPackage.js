c5f041421226d9cc96a20eee63d3965de9603100
sendevman <devman.sd@gmail.com>
2018-11-26 14:03:53 -0300

    Some improvements

--- src/screens/components/DirecTvPackage.js
+++ src/screens/components/DirecTvPackage.js
@@ -5,15 +5,16 @@
  */
 
 import React, { Component } from 'react';
-import { Image, Text, TextInput, TouchableOpacity, View, ScrollView, Dimensions } from 'react-native';
-import { FakeDirecTvPackages } from '../../store/DirecTvPackageFakeData';
-import { FakeDirecTvChannel } from '../../store/DirecTvChannelFakeData';
-import { Col, Row, Grid } from "react-native-easy-grid";
-var { height, width } = Dimensions.get('window');
-// My Styles
+import { Image, Text, TextInput, View, ScrollView } from 'react-native';
+import { Col, Grid, Row } from "react-native-easy-grid";
 import { Icon } from 'react-native-elements'
 
-import styles from '../css/DirecTvScreenCss';
+// My FakeData
+import { FakeDirecTvPackages } from '../../store/DirecTvPackageFakeData';
+import { FakeDirecTvChannel } from '../../store/DirecTvChannelFakeData';
+
+// My Styles
+import styles, { width } from '../css/DirecTvScreenCss';
 
 const package_channels = require('../../assets/channels/package_channels.json');
 const packages = require('../../assets/channels/packages.json');
@@ -145,10 +146,8 @@ class DirecTvPackage extends Component {
   render() {
 
     return (
-
-      <View style={{width, flex: 1, backgroundColor: 'white'}}>
-        
-        <View style={{ flex: 1, width: '100%', height: 70, alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+      <View style={{ width, flex: 1, backgroundColor: 'white' }}>
+        <View style={{ width: '100%', height: 70, alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
           <Image source={require('../../assets/images/files/dtv.logo.png')}/>
           <Text>Shop Now</Text>
         </View>
@@ -158,6 +157,7 @@ class DirecTvPackage extends Component {
             <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, backgroundColor: 'white'}}>
               <Icon name='search' color='#00aced' onPress={() => this.toggleSearch()} />
             </View>
+
             {
               this.state.packages.map((pkg, index) =>
                 <View key={index} style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, backgroundColor: pkg.color}}>
@@ -181,27 +181,32 @@ class DirecTvPackage extends Component {
         }
 
         <View style={{flex: 1, flexDirection: 'column'}}>
-          
           <View style={{width: '100%', height: 60, marginTop: 10}}>
             <View style={{width: '100%', height: 60, flex: 1, flexDirection: 'row'}}>
               <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#666', fontSize: 8}}>99% Local channels in USA</Text>
               </View>
+
               <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#00aced'}}>✔</Text>
               </View>
+
               <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#00aced'}}>✔</Text>
               </View>
+
               <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#00aced'}}>✔</Text>
               </View>
+
               <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#00aced'}}>✔</Text>
               </View>
+
               <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#00aced'}}>✔</Text>
               </View>
+
               <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', paddingTop: 10, borderLeftColor: '#fff', borderLeftWidth: 1, borderRightColor: '#d4d4d4', borderRightWidth: 1}}>
                 <Text style={{color: '#00aced'}}>✔</Text>
               </View>
